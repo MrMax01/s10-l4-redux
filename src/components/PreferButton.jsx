9ac4bd425896ef10b5b434f519519ca45ead4d51
MrMax01 <massimo.mauro00@gmail.com>
2023-09-14 16:26:14 +0200

    add count to cart

--- src/components/PreferButton.jsx
+++ src/components/PreferButton.jsx
@@ -1,14 +1,18 @@
 import { Container } from "react-bootstrap";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const PreferButton = () => {
+  const count = useSelector((state) => state.jobFavorites.content.length);
+
   return (
     <Container className="mt-5 text-end">
       <Link to="/prefer-list">
         <button type="button" className="btn btn-primary position-relative">
           Favorites
           <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            0<span className="visually-hidden">unread messages</span>
+            {count}
+            <span className="visually-hidden">unread messages</span>
           </span>
         </button>
       </Link>
--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -4,7 +4,6 @@ const initialState = {
   },
 
   isInFavorites: false,
-  admin: { content: "" },
 };
 
 const mainReducer = (state = initialState, action) => {
